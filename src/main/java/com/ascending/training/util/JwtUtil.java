5269c658e6cb84b9e46defb1fc5573142ee29117
Can Zhao <canzhao216@gmail.com>
2019-12-12 11:17:04 -0500

    update2

--- src/main/java/com/ascending/training/util/JwtUtil.java
+++ src/main/java/com/ascending/training/util/JwtUtil.java
@@ -36,4 +36,47 @@ public class JwtUtil {
         byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
-  
+        Claims claims = Jwts.claims();
+        claims.setId(String.valueOf(user.getId()));
+        claims.setIssuedAt(new Date(System.currentTimeMillis()));
+        claims.setIssuer(ISSUER);
+        claims.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME));
+
+        List<Role> roles = user.getRoles();
+        String allowedReadResources = "";
+        String allowedCreateResources = "";
+        String allowedUpdateResources = "";
+        String allowedDeleteResources = "";
+
+        //String allowedResource = roles.stream().map(role -> role.getAllowedResource()).collect(Collectors.joining(","));
+        //claims.put("allowedResource", allowedResource);
+
+        for (Role role : roles) {
+            if (role.isAllowedRead()) allowedReadResources = String.join(role.getAllowedResource(), allowedReadResources, ",");
+            if (role.isAllowedCreate()) allowedCreateResources = String.join(role.getAllowedResource(), allowedCreateResources, ",");
+            if (role.isAllowedUpdate()) allowedUpdateResources = String.join(role.getAllowedResource(), allowedUpdateResources, ",");
+            if (role.isAllowedDelete()) allowedDeleteResources = String.join(role.getAllowedResource(), allowedDeleteResources, ",");
+        }
+
+        claims.put("allowedReadResources", allowedReadResources.replaceAll(".$", ""));
+        claims.put("allowedCreateResources", allowedCreateResources.replaceAll(".$", ""));
+        claims.put("allowedUpdateResources", allowedUpdateResources.replaceAll(".$", ""));
+        claims.put("allowedDeleteResources", allowedDeleteResources.replaceAll(".$", ""));
+
+        //Set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setClaims(claims).signWith(signatureAlgorithm, signingKey);
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    public static Claims decodeJwtToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(token).getBody();
+
+        logger.debug("Claims: " + claims.toString());
+
+        return claims;
+    }
+}
