cbbabef9b438025e42a89ac548bcb28d8398b15f
Can Zhao <canzhao216@gmail.com>
2019-12-26 13:44:54 -0500

    add auth

---
+++ src/main/java/com/ascending/training/controller/Authentication.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.controller;
+
+import com.ascending.training.model.User;
+import com.ascending.training.service.UserService;
+import com.ascending.training.util.JwtUtil;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = {"/auth"})
+
