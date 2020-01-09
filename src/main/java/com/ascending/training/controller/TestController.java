f670a5e33cf1d146c6ba4b06d348b58dd17e6972
Can Zhao <canzhao216@gmail.com>
2020-01-09 15:08:07 -0500

    add test2

--- src/main/java/com/ascending/training/controller/TestController.java
+++ src/main/java/com/ascending/training/controller/TestController.java
@@ -19,5 +19,11 @@ import java.util.Map;
 public class TestController {
     @Autowired private Logger logger;
 
-    
+    @RequestMapping(value = "/path/{pathValue1}/{pathValue2}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String getPath(@PathVariable(name = "pathValue1") String p1, @PathVariable(name = "pathValue2") String p2) {
+        logger.info(String.format(">>>>>>>>>> Path value: %s, %s", p1, p2));
+        return p1 + ", " + p2;
+    }
+
+   
 }
