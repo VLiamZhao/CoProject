f2948b5f988b3ab044779c010420e49ea2670f15
Can Zhao <canzhao216@gmail.com>
2020-01-17 15:04:10 -0500

    add log

---
+++ src/main/java/com/ascending/training/filter/LogFilter.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@WebFilter(filterName = "logFilter", urlPatterns = {"/*"}, dispatcherTypes = {DispatcherType.REQUEST})
+
+
+}
