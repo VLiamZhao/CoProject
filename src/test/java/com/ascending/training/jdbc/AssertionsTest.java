50ab24255522ccc89e09b9890de594859a93ad1d
Can Zhao <canzhao216@gmail.com>
2020-02-03 15:13:39 -0500

    add test

---
+++ src/test/java/com/ascending/training/jdbc/AssertionsTest.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.jdbc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+    @Test
+    public void testAssertions() {
+        //test data
+        String str1 = new String ("abc");
+        String str2 = new String ("abc");
+        String str3 = null;
+        String str4 = "abc";
+        String str5 = "abc";
+        int val1 = 50;
+        int val2 = 50;
+        String[] expectedArray = {"one", "two", "three"};
+        String[] resultArray =  {"one", "two", "three"};
+
+   
+}
