2698a2e01ab0faf0ec64e1a2f567a91a98b601d5
Can Zhao <canzhao216@gmail.com>
2020-02-03 20:18:05 -0500

    add test1

--- src/test/java/com/ascending/training/jdbc/AssertionsTest.java
+++ src/test/java/com/ascending/training/jdbc/AssertionsTest.java
@@ -25,5 +25,29 @@ public class AssertionsTest {
         String[] expectedArray = {"one", "two", "three"};
         String[] resultArray =  {"one", "two", "three"};
 
-   
+        //Check that two objects are equal
+        assertEquals(str1, str2);
+
+        //Check that a condition is true
+        assertTrue(val1 <= val2);
+
+        //Check that a condition is false
+        assertFalse(val1 > val2);
+
+        //Check that an object isn't null
+        assertNotNull(str1);
+
+        //Check that an object is null
+        assertNull(str3);
+
+        //Check if two object references point to the same object
+        assertSame(str4,str5);
+
+        //Check if two object references not point to the same object
+        assertNotSame(str1, str3);
+        assertSame(str1, str2);
+
+        //Check whether two arrays are equal to each other.
+        assertArrayEquals(expectedArray, resultArray);
+    }
 }
