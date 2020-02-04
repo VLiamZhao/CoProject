36ab3806ce71dcce59635cf04a5ac9cc0f6b2dba
Can Zhao <canzhao216@gmail.com>
2020-02-04 15:10:25 -0500

    add test2

---
+++ src/test/java/com/ascending/training/jdbc/DepartmentDaoTest.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.jdbc;
+
+import com.ascending.training.model.Department;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DepartmentDaoTest {
+    private DepartmentDao departmentDao;
+
+    @Before
+    public void init() {
+        departmentDao = new DepartmentDao();
+    }
+
+  
+}
