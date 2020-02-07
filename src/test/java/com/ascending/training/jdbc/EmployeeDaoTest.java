79d84ec1dbdce3988fed24d519d7313321482023
Can Zhao <canzhao216@gmail.com>
2020-02-07 12:09:27 -0500

    add test4

---
+++ src/test/java/com/ascending/training/jdbc/EmployeeDaoTest.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.jdbc;
+
+import com.ascending.training.model.Employee;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+}
