55959c71e806e81ccfbc3d56aa5b78112367b09b
Can Zhao <canzhao216@gmail.com>
2020-02-12 19:28:17 -0500

    add empl2

--- src/test/java/com/ascending/training/repository/EmployeeDaoTest.java
+++ src/test/java/com/ascending/training/repository/EmployeeDaoTest.java
@@ -35,5 +35,13 @@ public class EmployeeDaoTest {
     public void init() {
     }
 
-   
+    @Test
+    public void getEmployees() {
+        List<Employee> employees = employeeDao.getEmployees();
+        int expectedNumOfDept = 4;
+        employees.forEach(em -> System.out.println(em.toString()));
+        Assert.assertEquals(expectedNumOfDept, employees.size());
+    }
+
+    
 }
