5e665b791e198da09cf67860f836b75582a8d9fd
Can Zhao <canzhao216@gmail.com>
2020-02-13 08:49:09 -0500

    add hib

---
+++ src/test/java/com/ascending/training/repository/HibernateMappingTest.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.repository;
+
+import com.ascending.training.model.Department;
+import com.ascending.training.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class HibernateMappingTest {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Test
+    public void mappingTest() {
+        String hql = "FROM Department";
+        List<Department> departments = null;
+
+       
+    }
+}
