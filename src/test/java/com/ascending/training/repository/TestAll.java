aaa8d476658b3c67f7eb0782e795d48092a18c71
Can Zhao <canzhao216@gmail.com>
2020-02-16 08:00:07 -0500

    add all

---
+++ src/test/java/com/ascending/training/repository/TestAll.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending.training.repository;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DepartmentDaoTest.class,
+        EmployeeDaoTest.class,
+        AccountDaoTest.class
+})
+
+public class TestAll {
+}
